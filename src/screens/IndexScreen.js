35c5ec01ab6621a2cd81d88fd064f374ea1ec710
mikeyim337 <mk337337@gmail.com>
2021-05-05 01:47:56 -0700

    done with delete functionality and learned about automating context generation

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,23 +1,46 @@
 import React, { useContext } from 'react';
-import { View, Text, StyleSheet, FlatList, Button } from 'react-native';
-import BlogContext from '../context/BlogContext';
+import { View, Text, StyleSheet, FlatList, Button, TouchableOpacity } from 'react-native';
+import { Context as BlogContext } from '../context/BlogContext';
+import { Feather } from '@expo/vector-icons';
 
 const IndexScreen = () => {
-    const { data, addBlogPost } = useContext(BlogContext);
+    const { state, addBlogPost, deleteBlogPost } = useContext(BlogContext);
 
     return ( <View>
-        <Text>Index Screen</Text>
+ 
         <Button title="Add Post" onPress={addBlogPost}/>
         <FlatList 
-            data={data}
+            data={state}
             keyExtractor={(blogPost) => blogPost.title}
             renderItem={({ item }) => {
-                return <Text>{item.title}</Text>
+                return <View style={styles.row}>
+                    <Text style={styles.title}>{item.title} - {item.id}</Text>
+                    <TouchableOpacity onPress={() => deleteBlogPost(item.id)}>
+                        <Feather name="trash" style={styles.icon} />
+
+                    </TouchableOpacity>
+                    </View>
             }} 
         />
     </View> );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    row: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        paddingVertical: 20,
+        paddingHorizontal: 10,
+        borderTopWidth: 1,
+        
+        borderColor: 'gray'
+    },
+    title: {
+        fontSize: 18
+    },
+    icon: {
+        fontSize: 24
+    }
+});
 
 export default IndexScreen;
